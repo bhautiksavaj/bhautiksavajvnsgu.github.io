ea79abcf68e47baa964957d65aac7af7b8a9d70c
savaj bhautik <91733445+bhautiksavaj@users.noreply.github.com>
2022-05-01 12:34:48 +0530

    add loader and custom font in logo

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -4,6 +4,13 @@
   $.fn.andSelf = function () {
     return this.addBack.apply(this, arguments);
   };
+  // Function to remove scroll bar during preload
+  $(window).on("load", function () {
+    setTimeout(function () {
+      $(".no-scroll-preload").css("overflow", "visible");
+    }, 1000);
+    $(".loader-container").fadeOut(2500);
+  });
 
   /* Loader Code Start */
   $(window).on("load", function () {
